f17fd0f9191121406d052b46e1804400ead5badc
tabby400 <tabbymargaret@gmail.com>
2024-02-07 12:44:51 +0300

    react components

---
+++ 0x03-React_component/README.md
@@ -0,0 +1,3 @@
+#### REACT COMPONENTS
+
+Components are the core building blocks in React applications, and they enable developers to create modular, maintainable, and scalable user interfaces.There are class components and functional components.
---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <Login />
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
