c75b8f839387b1ebfa5804c72d9cfc764ff2143d
tabby400 <tabbymargaret@gmail.com>
2024-02-21 10:17:29 +0000

    new immutable data structures

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,7 @@
+// Import list and map from the immutable library
+import { List, Map } from 'immutable';
+
+// joins elements of page 1 and 2 to a single list
+export const concatLists = (page1, page2) => List(page1).concat(List(page2));
+
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
