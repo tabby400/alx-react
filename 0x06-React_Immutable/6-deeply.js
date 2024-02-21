5d55684d19bd6e499bb8b1b83a46f462e3671901
tabby400 <tabbymargaret@gmail.com>
2024-02-21 10:25:36 +0000

    same values combine

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,6 @@
+// Importing map class from immutable library
+import { Map } from 'immutable';
+
+export default function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
+}
