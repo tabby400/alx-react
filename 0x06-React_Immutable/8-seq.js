61323c47933f022b07be7c5e7fb260202a6d2f9a
tabby400 <tabbymargaret@gmail.com>
2024-02-21 10:59:16 +0000

    score less than 70

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,33 +1,25 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
-  // Create a Seq from the provided object
   const seq = Seq(object);
 
-  // Filter the Seq to get students with a score greater than 70
   const filtered = seq.filter((student) => {
-    // Capitalize the first letter of the first name (Note: This line doesn't actually change the string)
     student.firstName.charAt(0).toUpperCase();
-    // Return true if the student's score is greater than 70
     return student.score > 70;
   });
 
-  // Define a function to capitalize the first letter of a string
   function capFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  // Convert the filtered Seq back to a regular JavaScript object
+// converting to a reg javascript object
   const JSObject = filtered.toJS();
 
-  // Iterate over the keys of the JSObject
   Object.keys(JSObject).map((key) => {
-    // Capitalize the first letter of the first and last names of each student
     JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
     JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
-    return JSObject[key]; // Return the updated student object
+    return JSObject[key];
   });
 
-  // Log the updated JSObject to the console
   console.log(JSObject);
 }
