bf3e48317240856cf1be82581cfdf209cbf387f6
tabby400 <tabbymargaret@gmail.com>
2024-02-21 09:59:46 +0000

    return value of object

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -3,6 +3,6 @@ import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);
-// uses getin to access the nested value in the immutable map
+
   return mappedObj.getIn(array, undefined);
 }
